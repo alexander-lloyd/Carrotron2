6852cb8896653ccde8a3232c78832967056f1197
hrb116 <hrb116@ic.ac.uk>
2017-09-23 15:35:55 +0100

    Initial progress on web app
    Signed-off-by: hrb116 <hrb116@ic.ac.uk>

--- carrotron-react/src/App.js
+++ carrotron-react/src/App.js
@@ -1,18 +1,80 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, NavItem, Button, Card, Row, Col } from 'react-materialize';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.drawRobot = this.drawRobot.bind(this);
+    this.drawBoundary = this.drawBoundary.bind(this);
+    this.drawDir = this.drawDir.bind(this);
+    this.drawObject = this.drawObject.bind(this);
+    this.sensorPoint = this.sensorPoint.bind(this);
+  }
+
+  componentDidMount() {
+    let canvas = ReactDOM.findDOMNode(this.refs.myCanvas);
+    this.ctx = canvas.getContext('2d');
+    this.drawRobot();
+    this.drawBoundary();
+    var bg = this.ctx.getImageData(0, 0, 800, 800);
+    this.drawObject(90, 2.314);
+  }
+
+  drawDir(theta) {
+    var r = 800;
+    this.ctx.beginPath();
+    this.ctx.setLineDash([4, 8]);
+    this.ctx.moveTo(400, 610);
+    this.ctx.lineTo(400 - r * Math.cos(theta * Math.PI / 180.0), 610 - r * Math.sin(theta * Math.PI / 180.0));
+    this.ctx.stroke();
+  }
+
+  drawObject(theta, distance) {
+    // scale distance into pixels
+    var dist = distance * 210;
+
+    // set size of dot
+    var pixelSize = 5;
+
+    this.ctx.fillStyle = 'rgb(30, 30, 30)';
+    this.ctx.fillRect(400 - dist * Math.cos(theta * Math.PI / 180.0), 610 - dist * Math.sin(theta * Math.PI / 180.0), pixelSize, pixelSize);
+  }
+
+  drawRobot() {
+    this.ctx.fillStyle = 'rgb(0, 0, 0)';
+    this.ctx.fillRect(375, 610, 50, 50);
+  }
+
+  drawBoundary() {
+    this.ctx.beginPath();
+    this.ctx.moveTo(400, 610);
+    this.ctx.lineTo(800, 800);
+    this.ctx.stroke();
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(400, 610);
+    this.ctx.lineTo(0, 800);
+    this.ctx.stroke();
+  }
+
+  sensorPoint(theta) {
+    this.drawDir(theta);
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+        <Navbar brand='Carrotron2'>
+        <NavItem href='about.html'>About</NavItem>
+        </Navbar>
+
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Live radar map of the Carrotron
         </p>
+        <canvas ref="myCanvas" width="800" height="800" style={{border: 1 + 'px solid'}} />
       </div>
     );
   }
