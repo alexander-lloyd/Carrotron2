33ad5db78521a4a1f6d072829ba77d9e69a0dc4d
Chris Lloyd <chris.lloyd2504@gmail.com>
2017-09-24 02:04:29 +0100

    added flask server to controller,config file started adding yaml maniplication

--- Carrotron2/config.py
+++ Carrotron2/config.py
@@ -1,2 +1,31 @@
-import yaml
-data = yaml.load("carrotron2.yaml")
+from yaml import load
+
+try:
+    from yaml import CLoader as Loader  # If its there, take advantage of the faster LibYAML loader
+except ImportError:
+    from yaml import Loader  # No LibYAML, use pure
+
+
+def load_yaml(str_or_buffer):
+    """
+    Load a yaml file or string and convert to dict
+    """
+    return load(str_or_buffer, Loader=Loader)
+
+
+def load_yaml_from_file(filename):
+    """
+    Loads an environment file from file system
+    """
+    return load_yaml(open(filename))
+
+def load_robot_config(filename):
+    yaml = load_yaml_from_file(filename)
+
+    r = Robot()
+
+
+    print(yaml)
+
+if __name__ == '__main__':
+    load_robot_config('carrotron2.yaml')
