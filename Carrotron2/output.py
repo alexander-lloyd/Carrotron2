58afb0c7133d709815dfd4a3f13ef60c22a52c77
Alexander Lloyd <alex.lloyd_1@hotmail.com>
2017-10-14 11:09:31 +0100

    Added LED class & changed motor driver

--- Carrotron2/output.py
+++ Carrotron2/output.py
@@ -8,6 +8,36 @@ class Output:
         self.board = board
         self.pins = pins
 
+class LED(Output):
+    pass
+
+class RBGLED(LED):
+    def __init__(self, board, pins):
+        super(RBGLED, self).__init__(board, pins)
+        self.red_pin = pins[0]
+        self.green_pin = pins[1]
+        self.blue_pin = pins[2]
+
+        self.board.set_pin_mode(self.red_pin, FirmataCommands.MODES['PWM'])
+        self.board.set_pin_mode(self.green_pin, FirmataCommands.MODES['PWM'])
+        self.board.set_pin_mode(self.blue_pin, FirmataCommands.MODES['PWM'])
+
+    def set_color(self, rgb):
+        """
+        Args:
+            rgb: Tuple of ints. between 0 and 255
+
+        Returns:
+
+        """
+        r, g, b = rgb
+        print(r, g, b)
+
+        self.board.analog_write(self.red_pin, r)
+        self.board.analog_write(self.green_pin, g)
+        self.board.analog_write(self.blue_pin, b)
+
+
 
 class Servo(Output):
     def __init__(self, board, pins, reverse=False):
@@ -45,6 +75,9 @@ class Motor(Output):
         pass
 
 class MotorDriveL9110(Motor):
+    BACKWARDS = 0
+    FORWARDS = 1
+
     def __init__(self, board, pins):
         super(MotorDriveL9110, self).__init__(board, pins)
         assert len(self.pins) == 2
@@ -56,19 +89,20 @@ class MotorDriveL9110(Motor):
             self.board.set_pin_mode(pin, FirmataCommands.MODES['PWM'])
 
         self._speed = 100
+        self._direction = MotorDriveL9110.FORWARDS # Default to forwards
 
     def forward(self):
-        print(self.motor_outputs['inputA'], self.motor_outputs['inputB'])
-        self.board.analog_write(self.motor_outputs['inputA'], self.speed)
         self.board.analog_write(self.motor_outputs['inputB'], 0)
 
     def backwards(self):
         self.board.analog_write(self.motor_outputs['inputA'], 0)
-        self.board.analog_write(self.motor_outputs['inputB'], self.speed)
+        # self.set_speed(self._speed)
+
 
     def stop(self):
         self.board.analog_write(self.motor_outputs['inputA'], 0)
         self.board.analog_write(self.motor_outputs['inputB'], 0)
+
 
     @property
     def speed(self):
@@ -76,7 +110,17 @@ class MotorDriveL9110(Motor):
 
     @speed.setter
     def speed(self, speed):
-        self._speed = min(speed, 255) # TODO Dont hard encode 255
+        self._speed = min(abs(int(speed)), 255) # TODO Dont hard encode 255
+
+
+        if speed > 0:
+            self._direction = MotorDriveL9110.FORWARDS
+            self.board.analog_write(self.motor_outputs['inputA'], self._speed)
+        else:
+            self._direction = MotorDriveL9110.BACKWARDS
+            self.board.analog_write(self.motor_outputs['inputB'], self._speed)
+
+
 
 class StepperMotor(Motor):
     INTERNAL_STEPPER_ID = 0
@@ -115,25 +159,25 @@ OUTPUTS = {
 if __name__ == '__main__':
     import time
     import logging
-    logging.getLogger('Carrotron2.board.arduino').setLevel(logging.DEBUG)
-    logging.getLogger('Carrotron2.board.arduino').addHandler(logging.StreamHandler())
+    # logging.getLogger('Carrotron2.board.arduino').setLevel(logging.DEBUG)
+    # logging.getLogger('Carrotron2.board.arduino').addHandler(logging.StreamHandler())
     from Carrotron2.board.arduino import ArduinoBoard
 
     board = ArduinoBoard('COM3')
     time.sleep(1)
-    s = ServoSG90(board, [8], reverse=True)
-    s.set_degrees(180)
-    time.sleep(1)
-    s.set_degrees(0)
-    print("Complete")
-    time.sleep(1)
-    s.set_degrees(90)
-    time.sleep(1)
-    # s.set_degrees()
-    print("Complete")
+    # s = ServoSG90(board, [8], reverse=True)
+    # s.set_degrees(180)
+    # time.sleep(1)
+    # s.set_degrees(0)
+    # print("Complete")
     # time.sleep(1)
     # s.set_degrees(90)
-    time.sleep(1)
+    # time.sleep(1)
+    # # s.set_degrees()
+    # print("Complete")
+    # # time.sleep(1)
+    # # s.set_degrees(90)
+    # time.sleep(1)
 
 
 
@@ -148,3 +192,28 @@ if __name__ == '__main__':
     # left_motor.backwards()
     # time.sleep(2)
     # left_motor.stop()
+
+    # led = RBGLED(board, [10,11,12])
+    # for _ in range(10):
+    #     led.set_color((255, 0, 0)) # red
+    #     time.sleep(1)
+    #     led.set_color((0,0,0)) # no colour
+    #     time.sleep(1)
+    #     led.set_color((0, 255, 0)) # green
+    #     time.sleep(1)
+    #     led.set_color((0,0,0)) # no colour
+    #     time.sleep(1)
+
+
+    #Motor control
+
+    motor = MotorDriveL9110(board, (4,5))
+    input()
+    motor.speed = 255
+    motor.forward()
+    speed = 255
+    while motor.speed > 1:
+        time.sleep(2)
+        print(motor.speed)
+        motor.speed /= 2
+    exit()
