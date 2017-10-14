49a64adb9dabb1db9712ce977ab7ff3b67e5a680
Alexander Lloyd <alex.lloyd_1@hotmail.com>
2017-10-14 15:14:21 +0100

    BUILD SCRIPT

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "Carrotron2",
+    version = "0.0.2",
+    author = "Alexander Lloyd",
+    author_email = "axl639@student.bham.ac.uk",
+    description = ("Declarative Robotics Library"),
+    keywords = "Robotics",
+    url = "https://github.com/alexander-lloyd/Carrotron2.git",
+    packages=find_packages(),
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+    ],
+)
